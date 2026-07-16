1a0b8137b5b964f7c385a9f0628e78360cee33dc
Maercio <maercio10@gmail.com>
2026-07-16 20:58:11 -0200

    feat: add S3 upload functionality and update Dockerfile for boto3 support

--- dags/data_extraction_fbref.py
+++ dags/data_extraction_fbref.py
@@ -1,9 +1,15 @@
 import pendulum
+import boto3
+import os
+import glob
+
 from airflow import DAG
+from airflow.decorators import task
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
 HOST_DATA_PATH = '/home/dev/football-data-pipeline/data'
+AIRFLOW_DATA_PATH = '/opt/airflow/football-data'
 
 with DAG(
     dag_id="data_extraction_fbref",
@@ -35,3 +41,40 @@ with DAG(
             )
         ]
     )
+
+    @task()
+    def upload_to_s3():
+        import re
+
+        S3_BUCKET = os.environ['S3_BUCKET']
+        S3_PREFIX = os.environ.get('S3_PREFIX', 'football/raw')
+
+        s3 = boto3.client('s3')
+
+        search_path = os.path.join(AIRFLOW_DATA_PATH, "dados_brasileirao", "*.parquet")
+        files = glob.glob(search_path)
+
+        if not files:
+            raise ValueError(f"Nenhum arquivo encontrado em {AIRFLOW_DATA_PATH}")
+
+        uploaded = []
+
+        for filepath in files:
+            filename = os.path.basename(filepath)
+            match = re.search(r'(\d{4}-\d{2}-\d{2})', filename)
+
+            if match:
+                data_arquivo = match.group(1)
+                ano, mes, dia = data_arquivo.split('-')
+                
+                s3_key = f"{S3_PREFIX}/ano={ano}/mes={mes}/dia={dia}/{filename}"
+            else:
+                s3_key = f"{S3_PREFIX}/unpartitioned/{filename}"
+
+            s3.upload_file(filepath, S3_BUCKET, s3_key)
+            uploaded.append(s3_key)
+
+        print(f"{len(uploaded)} arquivos enviados para s3://{S3_BUCKET}/{S3_PREFIX}/")
+        return uploaded
+    
+    extract_data >> upload_to_s3()
